0839db2ac1c12f697cf3462ce927534117244f25
itsmama22-ctrl <itsmama22-ctrl@users.noreply.github.com>
2025-11-08 15:22:41 +0100

    chore(seo): add travelpayouts verification script to head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,19 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <script
+          data-noptimize="1"
+          data-cfasync="false"
+          data-wpfc-render="false"
+          dangerouslySetInnerHTML={{
+            __html: `(function () {
+      var script = document.createElement("script");
+      script.async = 1;
+      script.src = 'https://tpembars.com/NDcyMDkx.js?t=472091';
+      document.head.appendChild(script);
+  })();`
+          }}
+        />
       </Head>
       <body className="bg-beige/30">
         <Main />
